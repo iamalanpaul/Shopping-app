a41bbf5aa4fa1ec67d01cdc29b618f87972e18d2
David-K-V <84168592+David-K-V@users.noreply.github.com>
2021-05-19 17:41:14 +0530

    Update detailpage.dart

--- lib/detailpage.dart
+++ lib/detailpage.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 
-class DetailPage extends StatefulWidget {
-  @override
-  _DetailPageState createState() => _DetailPageState();
-}
-
-class _DetailPageState extends State<DetailPage> {
+class DetailPages extends StatelessWidget {
+  final String img;
+  final String title;
+  final String price;
+  final BuildContext context;
+  
+  DetailPages({this.img, this.title, this.price, this.context});
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(child: Text(title)),
+    );
   }
 }
