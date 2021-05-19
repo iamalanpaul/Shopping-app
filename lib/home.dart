85e1a5f9234fc727721a0305ca53a641658aa573
David-K-V <84168592+David-K-V@users.noreply.github.com>
2021-05-19 16:02:28 +0530

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-//import 'package:flutter/src/widgets/bottom_navigation_bar_item.dart';
+
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
@@ -11,32 +11,54 @@ class _HomeState extends State<Home> {
 
     return Scaffold(
       backgroundColor: Colors.grey[100],
+      bottomNavigationBar: Container(
+        height: 60,
+        color: Colors.white,
+        child: Container(
+          decoration: BoxDecoration(
+          color: Colors.grey[300],
+          borderRadius: BorderRadius.only(topLeft:Radius.circular(25),topRight:Radius.circular(25),),
+          boxShadow: [
+            BoxShadow(
+                    blurRadius: 20,
+                    color: Colors.grey[600],
+                    spreadRadius: 1,
+          ),
+          ],
+        ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+                buildBottomContainer(Icons.favorite),
+                buildBottomContainer(Icons.search),
+                buildBottomContainer(Icons.person,isSelected: true,),
+                buildBottomContainer(Icons.shopping_cart,),
+                buildBottomContainer(Icons.settings),
+          ],
+          ),
+        ),
+      ),
       appBar: AppBar(
         backgroundColor: Colors.grey[600],
         elevation: 0,
         title: Text('Xpress Store',style: TextStyle(fontSize: 25.0,letterSpacing: 1, fontWeight: FontWeight.bold, color: Colors.black),),
+        centerTitle: true,
         leading: GestureDetector(
           onTap: (){},
           child: Icon(Icons.home),
         ),
-        actions: [
+       actions: [
           Padding(padding: EdgeInsets.only(right: 15.0),
           child: GestureDetector(
             onTap: () {},
-            child: Icon(Icons.search),
+            child: Icon(Icons.mic),
           ),
-          ),
-          Padding(padding: EdgeInsets.only(right: 15.0),
-            child: GestureDetector(
-              onTap: () {},
-              child: Icon(Icons.shopping_cart),
-            ),
+        
           ),
         ],
       ),
       body: Column(
-        //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        //crossAxisAlignment: CrossAxisAlignment.stretch,
+        
         children: [
           SizedBox(
             height: 10,
@@ -61,136 +83,147 @@ class _HomeState extends State<Home> {
                            borderRadius: BorderRadius.only(
                                topLeft: Radius.circular(25),
                              topRight: Radius.circular(25),
-                               bottomLeft: Radius.circular(30),
-                             bottomRight: Radius.circular(30),
-
                            ),
                            boxShadow: [
                              BoxShadow(
                                color: Colors.grey,
                                spreadRadius: 1,
                                blurRadius: 20,
-
                              ),
                            ]
                          ),
-                         child: Column(
-                             children:[
-                               SizedBox(height: 15),
-                               Row(
-                                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                                 children: [
-                                   buildColumn('Dresses',isSelected: true),
-                                   buildColumn('Pants'),
-                                   buildColumn('Watches'),
-                                   buildColumn('Sunglasses'),
-                                   buildColumn('Spray'),
-                                   buildColumn('Shoes'),
-                                 ],
-                               ),
-                               SizedBox(height:10),
-                              SingleChildScrollView (
-                                scrollDirection: Axis.horizontal,
-                                physics: BouncingScrollPhysics(),
-                                child: Row(
+                         child: SingleChildScrollView(
+                           physics: BouncingScrollPhysics(),
+                                  child: Column(
+                               children:[
+                                 SizedBox(height: 15),
+                                 Row(
+                                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                                    children: [
-                                     buildColumn1('assets/images/shirt1.png','Roadster','Rs 699'),
-                                     buildColumn1('assets/images/shirt2.png','Allen Solly','Rs 1599'),
-                                     buildColumn1('assets/images/shirt3.png','Levis','Rs 1299'),
-                                     buildColumn1('assets/images/dress1.png','Roadster','Rs 489'),
-                                     buildColumn1('assets/images/dress2.png','HRX','Rs 599'),
+                                     buildColumn('Dresses',isSelected: true),
+                                     buildColumn('Pants'),
+                                     buildColumn('Watches'),
+                                     buildColumn('Sunglasses'),
+                                     buildColumn('Spray'),
+                                     buildColumn('Shoes'),
                                    ],
                                  ),
-                               ),
-                               LineBar(),
-                               Padding(
-                                 padding: const EdgeInsets.all(30.0),
-                                                                child: Row(
-                                   children: [
-                                     Text('Offers',style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
-                                     Spacer(),
-                                     Text('View All',style: TextStyle(fontSize: 15,color: Colors.grey),),
-                                   ],
-                                 ),
-                               ),
-                               SingleChildScrollView(
-                                scrollDirection: Axis.horizontal,
-                                physics: BouncingScrollPhysics(),
-                                child: Row(
-                                   children: [
-                                     buildContainer1('assets/images/watches.png','Watches','10%-40%'),
-                                     buildContainer1('assets/images/spray.png','Perfume','20%'),
-                                     buildContainer1('assets/images/shoes.png','Shoes','60%'),
-                                     buildContainer1('assets/images/jeans.png','Jeans','30%-60%'),
-                                     buildContainer1('assets/images/sunglasses.png','Glasses','30%-50%'),
-                                     buildContainer1('assets/images/shirts.png','Shirts','70%'),
-                                   ],
+                                 SizedBox(height:10),
+                                SingleChildScrollView (
+                                  scrollDirection: Axis.horizontal,
+                                  physics: BouncingScrollPhysics(),
+                                  child: Row(
+                                     children: [
+                                       buildColumn1('assets/images/shirt1.png','Roadster','Rs 699'),
+                                       buildColumn1('assets/images/shirt2.png','Allen Solly','Rs 1599'),
+                                       buildColumn1('assets/images/shirt3.png','Levis','Rs 1299'),
+                                       buildColumn1('assets/images/dress1.png','Roadster','Rs 489'),
+                                       buildColumn1('assets/images/dress2.png','HRX','Rs 599'),
+                                     ],
                                    ),
-                               ),
-
-                             ],
-
+                                 ),
+                                 LineBar(),
+                                 Padding(
+                                   padding: const EdgeInsets.all(30.0),
+                                      child: Row(
+                                     children: [
+                                       Text('Todays Special Offers',style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
+                                     ],
+                                   ),
+                                 ),
+                                 SingleChildScrollView(
+                                  scrollDirection: Axis.horizontal,
+                                  physics: BouncingScrollPhysics(),
+                                  child: Row(
+                                     children: [
+                                       buildContainer1('assets/images/watches.png','Watches','25%'),
+                                       buildContainer1('assets/images/spray.png','Perfume','20%'),
+                                       buildContainer1('assets/images/shoes.png','Shoes','50%'),
+                                       buildContainer1('assets/images/jeans.png','Jeans','60%'),
+                                       buildContainer1('assets/images/sunglasses.png','Glasses','40%'),
+                                       buildContainer1('assets/images/shirts.png','Shirts','70%'),
+                                     ],
+                                     ),
+                                 ),
+                               ],
+                           ),
                          ),
                        ),
                ),
               ],
           ),
-
-
-
-
     );
+  }
+
+  Container buildBottomContainer(IconData icon,{isSelected=false}) {
+    return Container(
+              decoration: BoxDecoration(
+                color: isSelected?Colors.pink[100]:Colors.white,
+                shape: BoxShape.circle,
+                boxShadow:isSelected? [
+                  BoxShadow(color:Colors.grey[600],blurRadius:20,spreadRadius:2)
+                ]:[],
+              ),
+              height: 40,
+              width: 40,
+              child: Icon(icon,color: isSelected?Colors.white:Colors.black,),
+            );
   }
 
   Container buildContainer1(String img,String text,String dis) {
     return Container(
-              height: 125,
-              width: 200,
-              margin: EdgeInsets.only(left:20,),
-              child:Row(
-                  children: [
-                     ClipRRect(
-                        borderRadius: BorderRadius.circular(20.0),
-                        child: SizedBox(
-                          height: 150,
-                          width: 100,
-                          child: Image.asset(img,fit: BoxFit.cover,),
-                          )
-                     ),
-                     Padding(
-                         padding: const EdgeInsets.all(10.0),
-                         child: Column(
-                             crossAxisAlignment: CrossAxisAlignment.start,
-                             children: [
-                                 Text(text,style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-                                 Text(''),
-                                 Text(dis)
-                             ],
-                          ),
-                     ),
-                  ],
-              ),
-          );
+                                   height: 125,
+                                   width: 200,
+                                   margin: EdgeInsets.only(left:20,),
+                                   
+                                   child:Row(
+                                     children: [
+                                       ClipRRect(
+                                         borderRadius: BorderRadius.circular(20.0),
+                                         child: SizedBox(
+                                           height: 150,
+                                           width: 100,
+
+                                           child: Image.asset(img,fit: BoxFit.cover,),
+                                           )
+                                         ),
+                                       Padding(
+                                         padding: const EdgeInsets.all(10.0),
+                                           child: Column(
+                                           crossAxisAlignment: CrossAxisAlignment.start,
+                                           children: [
+                                             Text(text,style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                                             Text(''),
+                                             Text(dis)
+
+                                           ],
+                                         ),
+                                       ),
+
+                                     ],
+                                   ),
+                                 );
   }
 
   Padding buildColumn1(String img,String title,String price) {
     return Padding(
           padding: const EdgeInsets.all(18.0),
           child: Column(
-                   children: [
-                      Container(
-                            height: 140,
-                             width: 100,
-                             child :ClipRRect(
-                                 borderRadius:BorderRadius.circular(25),
-                                 child: Image.asset(img),
-                              ),
-                      ),
-                      SizedBox(height:10,),
-                      Text(title),
-                      Text(price),
-                   ],
-          ),
+                                     children: [
+                                       Container(
+                                         height: 140,
+                                         width: 100,
+                                         child :ClipRRect(
+                                           borderRadius:BorderRadius.circular(25),
+                                           child: Image.asset(img),
+                                           ),
+
+                                        ),
+                                        SizedBox(height:10,),
+                                        Text(title),
+                                        Text(price),
+                                     ],
+                                   ),
     );
   }
   FlatButton buildFlatButton(String text,{bool isSelected=false}){
