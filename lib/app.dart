c178ce46eeb081505bfcb3652a3251a442ff9854
Alan Paul <83277669+iamalanpaul@users.noreply.github.com>
2021-05-20 11:51:16 +0530

    Add files via upload
    added 2 dart files

---
+++ lib/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:shoppings_app/screens/login.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login Page',
+      home: Login(),
+    );
+  }
+}
